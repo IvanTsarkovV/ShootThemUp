7e1013ba7b2a4bbbd8ce0d3cd439bcd6d86ede16
ivan.tsarkov <ivan.tsarkov.it@gmail.com>
2023-01-21 18:54:26 +0300

    projectile

---
+++ Source/ShootThemUp/Public/Weapon/STUProjectile.h
@@ -0,0 +1,26 @@
+// Shoot Them Up Game. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "STUProjectile.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class SHOOTTHEMUP_API ASTUProjectile : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ASTUProjectile();
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, Category="Weapon")
+	USphereComponent* CollisionComponent;
+	
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+};
