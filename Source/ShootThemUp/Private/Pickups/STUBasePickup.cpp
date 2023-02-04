010cd2cdf6735d4bc3ec72f1aa0f4f367a924914
ivan.tsarkov <ivan.tsarkov.it@gmail.com>
2023-02-04 17:43:48 +0300

    pickups rotator

--- Source/ShootThemUp/Private/Pickups/STUBasePickup.cpp
+++ Source/ShootThemUp/Private/Pickups/STUBasePickup.cpp
@@ -25,12 +25,16 @@ void ASTUBasePickup::BeginPlay()
 	Super::BeginPlay();
 
 	check(CollisionComponent);
+
+	GenerateRotationYaw();
 }
 
 // Called every frame
 void ASTUBasePickup::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	AddActorLocalRotation(FRotator(0.0f, RotationYaw, 0.0f));
 }
 
 void ASTUBasePickup::NotifyActorBeginOverlap(AActor* OtherActor)
@@ -63,9 +67,17 @@ void ASTUBasePickup::PickupWasTaken()
 
 void ASTUBasePickup::Respawn()
 {
+	GenerateRotationYaw();
+
 	CollisionComponent->SetCollisionResponseToAllChannels(ECR_Overlap);
 	if (GetRootComponent())
 	{
 		GetRootComponent()->SetVisibility(true, true);
 	}
+}
+
+void ASTUBasePickup::GenerateRotationYaw()
+{
+	const auto Direction = FMath::RandBool() ? 1.0f : -1.0f;
+	RotationYaw = FMath::RandRange(1.0f, 2.0f) * Direction;
 }
