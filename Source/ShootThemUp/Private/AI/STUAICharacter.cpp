ba975bfc30bd1460cd45e28397551cf446f84b1a
ivan.tsarkov <ivan.tsarkov.it@gmail.com>
2023-02-16 16:08:12 +0300

    ai weapon

--- Source/ShootThemUp/Private/AI/STUAICharacter.cpp
+++ Source/ShootThemUp/Private/AI/STUAICharacter.cpp
@@ -4,14 +4,16 @@
 #include "AI/STUAICharacter.h"
 #include "AI/STUAIController.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Components/STUAIWeaponComponent.h"
 
-ASTUAICharacter::ASTUAICharacter(const FObjectInitializer& ObjInit):Super(ObjInit)
+ASTUAICharacter::ASTUAICharacter(const FObjectInitializer& ObjInit) : Super(
+	ObjInit.SetDefaultSubobjectClass<USTUAIWeaponComponent>("WeaponComponent"))
 {
 	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
 	AIControllerClass = ASTUAIController::StaticClass();
 
 	bUseControllerRotationYaw = false;
-	if(GetCharacterMovement())
+	if (GetCharacterMovement())
 	{
 		GetCharacterMovement()->bUseControllerDesiredRotation = true;
 		GetCharacterMovement()->RotationRate = FRotator(0.0f, 200.0f, 0.0f);
